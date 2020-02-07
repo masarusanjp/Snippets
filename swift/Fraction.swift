5957b3fd0e68366088696e35b0fc0e63d5bc8670
Masaru Ichikawa <masarusanplusplus@gmail.com>
2020-02-07 09:19:36 +0900

    Update Fraction.swift

--- swift/Fraction.swift
+++ swift/Fraction.swift
@@ -1,32 +1,12 @@
 import Foundation
 
-struct Fraction: Comparable, Equatable, Codable, Hashable {
-    static func < (lhs: Fraction, rhs: Fraction) -> Bool {
-        if lhs.decimal == rhs.decimal {
-            if lhs.denominator == rhs.denominator {
-                return lhs.numerator < rhs.numerator
-            } else {
-                return lhs.denominator < rhs.denominator
-            }
-        } else {
-            return lhs.decimal < rhs.decimal
-        }
-    }
-
-    static func == (lhs: Fraction, rhs: Fraction) -> Bool {
-        lhs.decimal == rhs.decimal
-    }
-
+struct Fraction {
     var numerator: Int
     var denominator: Int
     var decimal: Decimal {
         Decimal(numerator) / Decimal(denominator)
     }
-    init(numerator: Int, denominator: Int) {
-        self.numerator = numerator
-        self.denominator = denominator
-    }
-    
+
     var reduction: Fraction {
         let c = Self.gcd(a: numerator, b: denominator)
         if c > 1 {
@@ -54,5 +34,28 @@ struct Fraction: Comparable, Equatable, Codable, Hashable {
         }
         
         return abs(j)
+    }
+}
+
+extension Fraction: Codable {}
+extension Fraction: Hashable {}
+
+extension Fraction: Equatable {    
+    static func == (lhs: Fraction, rhs: Fraction) -> Bool {
+        lhs.decimal == rhs.decimal
+    }
+}
+
+extension Fraction: Comparable {
+    static func < (lhs: Fraction, rhs: Fraction) -> Bool {
+        if lhs.decimal == rhs.decimal {
+            if lhs.denominator == rhs.denominator {
+                return lhs.numerator < rhs.numerator
+            } else {
+                return lhs.denominator < rhs.denominator
+            }
+        } else {
+            return lhs.decimal < rhs.decimal
+        }
     }
 }
